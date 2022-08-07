ba17c52cf8979a98a26903c150dbd99c3c68b552
KarinaBashkatova <mirzoevakarina53@gmail.com>
2022-08-07 14:22:33 +0300

    Initial commit

---
+++ src/main/java/Cat.java
@@ -0,0 +1,20 @@
+/**
+ * @author Karina Bashkatova.
+ */
+public class Cat {
+    public Cat() {
+
+    }
+    private String name;
+    private String personName;
+
+    public String getName () {
+        return name;
+    }
+
+    public void setName (String name) {
+        this.name = name;
+    }
+
+
+}
